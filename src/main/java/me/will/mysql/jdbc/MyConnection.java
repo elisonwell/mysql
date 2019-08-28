a001fa376d26e1b1b1067f62e2d8c887d7192cf8
DuWill <du19491001>
2019-08-28 22:58:03 +0800

    test

--- src/main/java/me/will/mysql/jdbc/MyConnection.java
+++ src/main/java/me/will/mysql/jdbc/MyConnection.java
@@ -4,25 +4,30 @@ import java.sql.*;
 
 public class MyConnection {
 
-    static {
+    //无需显示加载，jdbc会自动根据java.sql.Driver配置的驱动来加载驱动
+    /*static {
         try {
             Class.forName("com.mysql.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
-    }
+    }*/
 
     private Connection connection;
 
     public MyConnection() throws SQLException {
         connection = DriverManager
-                .getConnection("jdbc:mysql://localhost:3306/test", "root", "root");
+                .getConnection("jdbc:mysql://localhost:3306/test?serverTimezone=GMT%2B8", "root", "root");
     }
-
 
     public boolean insert(String sql) throws SQLException {
         Statement statement = connection.createStatement();
         return statement.execute(sql);
+    }
+
+    public static void main(String[] args) throws SQLException {
+        MyConnection connection = new MyConnection();
+        connection.insert("insert into t_student(name) values('will')");
     }
 
 
