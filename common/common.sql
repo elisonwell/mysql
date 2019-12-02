f3c025889e79f19561ce505c34ad39499f4c9ba4
elisonwell <elisonwell@163.com>
2019-12-02 22:25:36 +0800

    mysqlslap

--- common/common.sql
+++ common/common.sql
@@ -7,3 +7,9 @@ kill <pid>
 -- 查看当前会话隔离级别
 show variables like 'transaction_isolation'; -- mysql8
 show variables like 'tx_isolation'; -- mysql8以前
+
+-- 查看最大连接数
+show variables like 'max_connections';
+
+-- 查看mysql状态
+status
